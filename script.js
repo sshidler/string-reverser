ad3598a52bf2d02715d03f201db91fe0ff72f56c
samantha.shidler <samantha.shidler@gmail.com>
2017-06-23 15:26:46 -0400

    add additional functionality

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
 var $textInput = $("#textInput"),
     $button = $("#doAction"),
-    $display =$("#display");
+    $display = $("#display"),
+    $options = $("input[type=radio]"),
+    method = "reverse";
+
+$options.on("click", function(){
+  method = $(this).val();
+});
+
 
 $button.on("click", function (){
-  let word = $textInput.val();
   
-  let reversedWord = word.split("").reverse().join("");
+  if (method === "reverse") {
+     let word = $textInput.val();
+     let reversedWord = word.split("").reverse().join("");
+     $display.text(reversedWord);
+  } 
   
+   if (method === "random") {
+     let word = $textInput.val();
+     let randomizedWord = word.split("").sort(function(a, b) {
+       return 0.5 - Math.random();
+     });
+     $display.text(randomizedWord.join(""));
+   }  
   
-  $display.text(reversedWord);
-  
-});
+   if (method === "alphabetize") {
+     let word = $textInput.val();
+     let alphabetizedWord = word.split("").sort().join("");
+     $display.text(alphabetizedWord);
+   }
+ });
